d093e729d5e5db321619f37e9c8b9b641caab9c9
SamyAB <aitb.samy@gmail.com>
2016-03-25 18:01:37 +0100

    ajout du tracking des dictionnaires utilisés, et création du bolt POSTagBolt

---
+++ src/dz/pfe/storm/POSTagBolt.java
@@ -0,0 +1,14 @@
+package dz.pfe.storm;
+
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+
+import java.util.Map;
+
+public class POSTagBolt extends BaseRichBolt{
+    
+}
