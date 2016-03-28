eb8e77375288e1c70866805ca7b974f089abf3b5
SamyAB <aitb.samy@gmail.com>
2016-03-28 22:22:44 +0200

    création du bolt ScoreBolt, implémentation basique de celui ci et du POSTagBolt

--- src/dz/pfe/storm/POSTagBolt.java
+++ src/dz/pfe/storm/POSTagBolt.java
@@ -7,8 +7,36 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 
+import twitter4j.Status;
+
 import java.util.Map;
 
 public class POSTagBolt extends BaseRichBolt{
-    
+  OutputCollector collector;
+
+  @Override
+  public void prepare(Map map,TopologyContext topologyContext, OutputCollector outputCollector){
+    this.collector = outputCollector;
+  }
+
+  @Override
+  public void execute(Tuple tuple){
+    //Récupération des informations reçues dans le tuple
+    //Dans ce cas : le status et une chaine de caractère représentant le text du tweet sans acronymes
+    Status tweet = (Status) tuple.getValue(0);
+    String tweet_text = (String) tuple.getValue(0);
+
+    //À ce point il faudrait utiliser le tagger pour tag tweet_text
+    //Je ne sais pas quel sera le type de la collection des tweets taggés
+    //Supposant que c'est un Map …
+    Map mots_tags = new Map();
+
+    //Emettre pour le moment le status et les mots taggés
+    this.collector.emit(tweet,mots_tags);
+  }
+
+  @Override
+  public void declareOutputFields(OutputFieldsDeclarer declarer){
+    declarer.declare(new Fields("tweet","mots_tags"));
+  }
 }
---
+++ src/dz/pfe/storm/ScoreBolt.java
@@ -0,0 +1,44 @@
+package dz.pfe.storm;
+
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+
+import twitter4j.Status;
+
+import java.util.Map;
+
+public class ScoreBolt.java extends BaseRichBolt{
+  OutputCollector collector;
+
+  @Override
+  public void prepare(Map map, TopologyContext topologyContext,OutputCollector outputCollector){
+    this.collector = outputCollector;
+    //Ouverture des dictionnaires de scores
+  }
+
+  @Override
+  public void execute(Tuple tuple){
+    //Récupération des informations reçues dans le tuple
+    //Pour le moment on ne suppose que le status tweet
+    Status tweet = (Status) tuple.getValue(0);
+
+    //Initialisation du score à 0
+    float score = 0.0;
+
+    //Utilisation des dictionnaires pour donner un score au tweets reçus
+    //Pour le moment on suppose que le score est 0
+
+    //Emettre le status et son score
+    this.collector.emit(tweet,score);
+  }
+
+  @Override
+  public void declareOutputFields(OutputFieldsDeclarer declarer){
+    declarer.declare(new Fields("tweet","score"))
+  }
+
+}
