07d9e4c6e10cde0ab69c71cf7fb775aaa7df07e6
SamyAB <aitb.samy@gmail.com>
2016-03-29 12:09:25 +0200

    Quelques corrections sur les noms de variables et les commentaires

--- src/dz/pfe/storm/OpinionTweetTopology.java
+++ src/dz/pfe/storm/OpinionTweetTopology.java
@@ -14,9 +14,9 @@ public class OpinionTweetTopology{
     //Ne pas oublier de remplacer par les clés twitter
     TweetSpout tweetSpout = new TweetSpout("conskey","secret","accToken","accTokenSecret");
 
-    //Création de la topologie
+    //Création du schéma de la topologie
     //Cette topologie est appelée à changer avec l'insertion d'au moins un autre bolt
-    //parallèlisme de 1
+    //TweetSpout avec un parallèlisme de 1
     topologie.setSpout("tweetSpout",tweetSpout,1);
     //Attacher l'AcronymeBolt au spout via shuffle avec parallèlisme de 10
     topologie.setBolt("acronymeBolt",new AcronymeBolt(),10).shuffleGrouping("tweetSpout");
@@ -33,25 +33,25 @@ public class OpinionTweetTopology{
     //Activer le mode de debug
     configuration.setDebug(true);
 
-    //Configuration pour cluster si presence d'arguement en entrée
+    //Configuration pour cluster si presence d'arguements en entrée
     if(args != null && args.length > 0){
       //Configuration du nombre de workers par noeud de la topologie
       configuration.setNumWorkers(3);
 
       //Lancement de la topologie sur le cluster
-      StormSubmitter.submitTopology(args[0],conf,topologie.createTopology());
+      StormSubmitter.submitTopology(args[0],configuration,topologie.createTopology());
 
     } else {
-      //Executer la topologie sur un cluster similé localement
+      //Executer la topologie sur un cluster simulé localement
 
       //Configuration du nombre de threads
-      conf.setMaxTaskParallelism(3);
+      configuration.setMaxTaskParallelism(3);
 
       //Instensiation d'un cluster local
       LocalCluster cluster = new LocalCluster();
 
       //lacement de la topologie sur le cluster local
-      cluster.submitTopology("opinionTweetTopology",conf,topologie.createTopology());
+      cluster.submitTopology("opinionTweetTopology",configuration,topologie.createTopology());
 
       // Pour le moment on va laisser la topologie tourner pendant 15 secondes
       Utils.sleep(15000);
