e8e859ad7e95d197f2c0a85f7d88046ccf74bf13
SamyAB <aitb.samy@gmail.com>
2016-04-06 10:43:51 +0100

    Parcours de mots pour supprimer dans mots_tags après trouver une expressions

--- src/dz/pfe/storm/PreExpressionBolt.java
+++ src/dz/pfe/storm/PreExpressionBolt.java
@@ -80,7 +80,7 @@ public class PreExpressionBolt extends BaseRichBolt{
 					if(exp.equals(String.join(" ", expMot))){
 						MotTag mt = new MotTag(exp_,"EXP");
             mots_tags.set(i,mt);
-						for(int j=i+1;  j<i+tailleExp-1;j++){
+						for(int j=i+1;j<i+tailleExp;j++){
 							mots_tags.remove(j);
 						}
 					}
