de162dc79416c549a3eb6747e11610da22f1d2db
SamyAB <aitb.samy@gmail.com>
2016-05-04 20:24:57 +0100

    ajout d'un fichier css pour le projet

--- webapp/test.php
+++ webapp/test.php
@@ -2,36 +2,44 @@
 <head>
   <title>Formulaire</title>
   <meta charset = "utf-8"/>
+  <link rel="stylesheet" type="text/css" href="webapp.css" />
+  <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <header>
     <p>
       Ceci est la page d'acceuil du projet : Suivi de la propagation des opinions en temps réel
     </p>
+    <nav>
+      <ul>
+        <li>
+          <a class="active" href="test.php">Acceuil</a>
+        </li>
+        <li>
+          <a href="#">Mémoire</a>
+        </li>
+        <li>
+          <a href="#">Code source du projet</a>
+        </li>
+        <li id="lastnav">
+          <a href="#">À propos</a>
+        </li>
+      </ul>
+    </nav>
   </header>
-  <nav>
-    <ul>
-      <li>
-        <a href="memoire">Mémoire</a>
-      </li>
-      <li>
-        <a href="code">Code source du projet</a>
-      </li>
-      <li>
-        <a href="about">À propos</a>
-      </li>
-    </ul>
-  </nav>
+  <h3>Mot-clefs</h3>
+  <p>
+    Donnez dans le formulaire suivant les mots-clefs à viser pour la suivie des opinions sur twitter
+  </p>
   <section id="recherche">
-    <p>
-      Entrez vos mot-clés dans le champs suivant et validez
-    </p>
     <form action="cible.php" method="post">
+      <label for="motCles">Mots-clefs</label>
       <input type="text" name="motCles" />
       <input type="submit" value="Valider" />
     </form>
   </section>
   <section id="info projet">
+    <h3>Code source</h3>
     Retrouvez le code source du projet sur <a href="https://github.com/SamyAB/OpinionStormTwitter" >github</a>
   </section>
   <footer>
