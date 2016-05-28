54f5313d8df5a5e742ba76b32ddca4b703ed3748
SamyAB <aitb.samy@gmail.com>
2016-05-28 11:27:18 +0100

    ajout d'un reporte bolt, enregistrement des id des tweets et de leurs scores dans redis

---
+++ src/dz/pfe/storm/ReportBolt.java
@@ -0,0 +1,54 @@
+package dz.pfe.storm;
+
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+
+import twitter4j.Status;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+
+
+public class ReportBolt extends BaseRichBolt{
+  private OutputCollector collector;
+  private RedisConnection redis;
+
+  @Override
+  public void prepare(Map map,TopologyContext topologyContext, OutputCollector outputCollector){
+    this.collector = outputCollector;
+
+    //Ouvrire une connexion redis et s'y connecter
+    RedisClient client = new RedisClient("localhost",6379);
+    this.redis = client.connect();
+  }
+
+  @Override
+  public void execute(Tuple tuple){
+    if(tuple!=null){
+      //Récupération des informations reçues dans le tuple
+      //Dans ce cas : Les mots clés, le status et le score de ce status
+      String[] motCles = (String[]) tuple.getValue(0);
+      Status tweet = (Status) tuple.getValue(1);
+      float score = (float) tuple.getValue(2);
+
+      //Création d'une chaine de caractères contenant l'ID du tweet et son score
+      String aEnvoyer = tweet.getId() + " | " + score;
+
+      //Enregistrement du tweet dans redis dans la liste TweetsList
+      redis.lpush("TweetsList",aEnvoyer);
+    }
+  }
+
+  @Override
+  public void declareOutputFields(OutputFieldsDeclarer declarer){
+    //Il y a rien à envoyer
+  }
+}
